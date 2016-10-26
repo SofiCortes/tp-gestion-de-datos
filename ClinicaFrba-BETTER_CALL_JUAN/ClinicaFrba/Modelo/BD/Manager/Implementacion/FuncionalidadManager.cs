c26723b02e8ab38e2ff2e907b54b5c1827772c06
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-10-26 18:16:10 -0300

    Agrego la clase FuncionalidadManager

---
+++ ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Modelo/BD/Manager/Implementacion/FuncionalidadManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaFrba.Modelo.BD.Manager.Implementacion
+{
+    class FuncionalidadManager : AbstractManager
+    {
+        public FuncionalidadManager() : base(new ConexionBD())
+        {
+        }
+
+        internal List<Funcionalidad> getTodasLasFuncionalidades()
+        {
+            List<Funcionalidad> funcionalidades = new List<Funcionalidad>();
+            try
+            {
+                this.openDB();
+
+                SqlCommand procedure = this.createCallableProcedure("BETTER_CALL_JUAN.Procedure_Get_Todas_Las_Funcionalidades", null);
+                SqlDataReader sqlReader = procedure.ExecuteReader();
+
+                if (sqlReader.HasRows)
+                {
+                    while (sqlReader.Read())
+                    {
+                        Funcionalidad func = new Funcionalidad();
+                        func.id = sqlReader.GetDecimal(0);
+                        func.descripcion = sqlReader.GetString(1);
+
+                        funcionalidades.Add(func);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                funcionalidades = null;
+            }
+            finally
+            {
+                this.closeDB();
+            }
+            return funcionalidades;
+        }
+    }
+}
