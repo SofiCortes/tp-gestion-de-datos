d47a4c1363745724195ec3f48a4d1a4e3a6df4b2
JulianEzequiel <julian.e.pico@gmail.com>
2016-10-30 17:04:40 -0300

    Listado de Afiliados con Filtros

--- ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Modelo/BD/Manager/Implementacion/PacienteManager.cs
+++ ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Modelo/BD/Manager/Implementacion/PacienteManager.cs
@@ -64,5 +64,60 @@ namespace ClinicaFrba
             }
             return pacientes;
         }
+
+        internal List<Paciente> buscarAfiliadosPorNombreYPlan(string query, decimal planCode)
+        {
+            List<Paciente> pacientes = new List<Paciente>();
+            try
+            {
+                ParametroParaSP parametro1 = new ParametroParaSP("nombre_apellido", SqlDbType.VarChar, query);
+                ParametroParaSP parametro2 = new ParametroParaSP("plan_codigo", SqlDbType.Decimal, planCode);
+                List<ParametroParaSP> parametros = new List<ParametroParaSP>();
+                parametros.Add(parametro1);
+                parametros.Add(parametro2);
+
+                this.openDB();
+
+                SqlCommand procedure = this.createCallableProcedure("BETTER_CALL_JUAN.Procedure_Buscar_Afiliados_Filtros", parametros);
+                SqlDataReader sqlReader = procedure.ExecuteReader();
+
+                if (sqlReader.HasRows)
+                {
+                    while (sqlReader.Read())
+                    {
+                        Paciente paciente = new Paciente();
+                        paciente.id = sqlReader.GetDecimal(0);
+                        paciente.nroRaiz = !sqlReader.IsDBNull(1) ? sqlReader.GetDecimal(1) : 0;
+                        paciente.nroPersonal = sqlReader.GetDecimal(2);
+                        paciente.nombre = sqlReader.GetString(3);
+                        paciente.apellido = sqlReader.GetString(4);
+                        paciente.tipoDoc = sqlReader.GetString(5);
+                        paciente.nroDoc = sqlReader.GetDecimal(6);
+                        paciente.direccion = sqlReader.GetString(7);
+                        paciente.telefono = sqlReader.GetDecimal(8);
+                        paciente.mail = sqlReader.GetString(9);
+                        paciente.fechaNacimiento = sqlReader.GetDateTime(10);
+                        paciente.sexo = !sqlReader.IsDBNull(11) ? sqlReader.GetString(11).ElementAt(0) : 'N';
+                        paciente.estadoCivil = !sqlReader.IsDBNull(12) ? sqlReader.GetString(12) : "";
+                        paciente.cantidadFamiliares = sqlReader.GetInt32(13);
+                        paciente.planMedicoCod = sqlReader.GetDecimal(14);
+                        paciente.habilitado = sqlReader.GetBoolean(15);
+                        paciente.nroUltimaConsulta = sqlReader.GetDecimal(16);
+                        paciente.usuarioId = !sqlReader.IsDBNull(17) ? sqlReader.GetDecimal(17) : 0;
+
+                        pacientes.Add(paciente);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                pacientes = null;
+            }
+            finally
+            {
+                this.closeDB();
+            }
+            return pacientes;
+        }
     }
 }
