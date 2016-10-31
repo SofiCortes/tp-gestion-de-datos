74113f27a576b1754aa387b87fe4e6808016a8be
Marco Di Croce <marcodicro@hotmail.com>
2016-10-31 00:39:55 -0300

    subo mas cambios al bosquejo pedir turnoo

--- ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Pantallas/Pedir Turno/PedirTurnoController.cs
+++ ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Pantallas/Pedir Turno/PedirTurnoController.cs
@@ -15,5 +15,22 @@ namespace ClinicaFrba
         {
             this.form = form;
         }
+
+        internal void llenarComboEspecialidades()
+        {
+            EspecialidadMedicaManager especialidadMedicaManager = new EspecialidadMedicaManager();
+            List<Especialidad> especialidades = especialidadMedicaManager.findAll();
+
+            if (especialidades != null)
+            {
+                this.form.llenarComboEspecialidades(especialidades);
+            }
+            else
+            {
+                this.form.showErrorMessage("Ocurrio un error al obtener las Especialidades.");
+            }
+        }
+
+
     }
 }
