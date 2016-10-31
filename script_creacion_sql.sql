ff62a76f132247b0d660ab9901a0aa421cc5e09f
Sofia Cortes <sofiavcortes@gmail.com>
2016-10-30 22:22:28 -0300

    Agrego SP Cancelar_Turno_Dia_Profesional

--- script_creacion_sql.sql
+++ script_creacion_sql.sql
@@ -893,6 +893,10 @@ IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'BETTER_CALL_J
 	DROP PROCEDURE BETTER_CALL_JUAN.Procedure_Buscar_Medicos_Filtros
 GO
 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'BETTER_CALL_JUAN.Procedure_Cancelar_Turno_Dia_Profesional'))
+	DROP PROCEDURE BETTER_CALL_JUAN.Procedure_Cancelar_Turno_Dia_Profesional
+GO
+
 ------------------------------------------
 CREATE PROCEDURE [BETTER_CALL_JUAN].[Procedure_Buscar_Medicos_Filtros] 
 (@matricula NUMERIC(18,0),@tipo_doc VARCHAR(100), @nro_doc NUMERIC(18,0),@nombre VARCHAR(255), @apellido VARCHAR(255),@especialidad_codigo NUMERIC(18,0))
@@ -1435,6 +1439,17 @@ BEGIN
 	WHERE descripcion like '%' + @descripcion + '%'
 END
 GO
+
+CREATE PROCEDURE [BETTER_CALL_JUAN].[Procedure_Cancelar_Turno_Dia_Profesional] (@fecha DATETIME, @matricula NUMERIC(18,0), @tipo NUMERIC(18,0), @motivo VARCHAR(255))
+AS
+BEGIN
+	INSERT INTO Cancelaciones(tipo_cancelacion_id, motivo, turno_numero, hecha_por_paciente)
+	SELECT @tipo, @motivo, numero, 0
+	FROM Turnos t JOIN Medicos_Especialidades me ON (t.medico_especialidad_id = me.id) JOIN Medicos m ON (me.medico_id = m.matricula)
+	WHERE matricula = @matricula AND DATEDIFF(day,fecha_hora,@fecha) = 0	
+END
+GO
+
 
 /** TOP 5 **/
 
