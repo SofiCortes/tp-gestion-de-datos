1bd0589fed82cdd915f248828455af7ad2f31dee
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-11-02 01:11:58 -0300

    Muestro dias de la semana para registrar agenda

--- ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Pantallas/Abm Profesional/Agenda Profesional.Designer.cs
+++ ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Pantallas/Abm Profesional/Agenda Profesional.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.comboEspecialidad = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.diasPanel = new System.Windows.Forms.Panel();
+            this.buttonAceptar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboEspecialidad
@@ -49,11 +52,40 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Especialidad:";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Días:";
+            // 
+            // diasPanel
+            // 
+            this.diasPanel.AutoScroll = true;
+            this.diasPanel.Location = new System.Drawing.Point(10, 57);
+            this.diasPanel.Name = "diasPanel";
+            this.diasPanel.Size = new System.Drawing.Size(262, 220);
+            this.diasPanel.TabIndex = 8;
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Location = new System.Drawing.Point(99, 283);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAceptar.TabIndex = 9;
+            this.buttonAceptar.Text = "Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            // 
             // AgendaProfesional
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(282, 314);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.diasPanel);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboEspecialidad);
             this.Name = "AgendaProfesional";
@@ -67,5 +99,8 @@
 
         private System.Windows.Forms.ComboBox comboEspecialidad;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel diasPanel;
+        private System.Windows.Forms.Button buttonAceptar;
     }
 }
--- ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Pantallas/Abm Profesional/Agenda Profesional.cs
+++ ClinicaFrba-BETTER_CALL_JUAN/ClinicaFrba/Pantallas/Abm Profesional/Agenda Profesional.cs
@@ -13,6 +13,7 @@ namespace ClinicaFrba
     public partial class AgendaProfesional : Form
     {
         private AgendaProfesionalController controller;
+        private List<CheckBox> CBLDias;
 
         public AgendaProfesional()
         {
@@ -25,7 +26,33 @@ namespace ClinicaFrba
         internal void showRegistroHorario(Medico medico, List<Especialidad> especialidades)
         {
             this.llenarComboEspecialidades(especialidades);
+            this.mostrarDiasDeLaSemana();
             this.Show();
+        }
+
+        private void mostrarDiasDeLaSemana()
+        {
+            Dictionary<String,int> dias = new Dictionary<String, int>();
+            dias.Add("Lunes",1);
+            dias.Add("Martes",2);
+            dias.Add("Miercoles",3);
+            dias.Add("Jueves",4);
+            dias.Add("Viernes",5);
+            dias.Add("Sabado",6);
+
+            this.CBLDias = new List<CheckBox>();
+            int ycoords = 0;
+         
+            foreach(KeyValuePair<String, int> dia in dias){
+                    CheckBox cbf = new CheckBox();
+                    cbf.Width = diasPanel.Width;
+                    cbf.Location = new Point(10, ycoords);
+                    cbf.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    cbf.Text = dia.Key;
+                    diasPanel.Controls.Add(cbf);
+                    ycoords += 30;
+                    this.CBLDias.Add(cbf);
+                };
         }
 
         private void llenarComboEspecialidades(List<Especialidad> especialidades)
